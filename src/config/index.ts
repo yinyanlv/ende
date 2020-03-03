131209226aeef41a2938d5e5d906679370eead73
yinyanlv <yanlv.yin@gmail.com>
2020-03-03 12:07:47 +0800

    edit: port

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,1 @@
-export const API_PREFIX = process.env.REACT_APP_API_MODE === 'test' ? 'http://192.168.1.71:8082' : '/api';
+export const API_PREFIX = process.env.REACT_APP_API_MODE === 'test' ? 'http://192.168.1.71:8083' : '/api';
